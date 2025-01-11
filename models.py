1bd5e3fd068047a3be2c9f5fbc97ac8abb6ce5e1
mgilady99 <callilcoil@gmail.com>
2025-01-11 14:15:12 +0200

    Update models.py

--- models.py
+++ models.py
@@ -1,29 +1,28 @@
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-from flask_sqlalchemy import SQLAlchemy
 
+# Initialize the database object
 db = SQLAlchemy()
 
-class SearchCriteria(https://www.globes.co.il/GlobesBoard/			):
+class SearchCriteria(db.Model):
+    __tablename__ = 'search_criteria'
+
     id = db.Column(db.Integer, primary_key=True)
     criteria_name = db.Column(db.String(255), nullable=False)
     # Add other fields as required
 
-
+    def __repr__(self):
+        return f'<SearchCriteria {self.criteria_name}>'
 
     def to_dict(self):
         return {
             'id': self.id,
-            'title': self.title,
-            'price': self.price,
-            'location': self.location,
-            'url': self.url,
-            'image_url': self.image_url,
-            'source': self.source,
-            'date_scraped': self.date_scraped.isoformat() if self.date_scraped else None
+            'criteria_name': self.criteria_name
         }
 
 class News(db.Model):
+    __tablename__ = 'news'
+
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(500))
     description = db.Column(db.Text)
@@ -31,6 +30,9 @@ class News(db.Model):
     image_url = db.Column(db.String(1000))
     source = db.Column(db.String(100))
     date_scraped = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f'<News {self.title}>'
 
     def to_dict(self):
         return {
@@ -43,7 +45,9 @@ class News(db.Model):
             'date_scraped': self.date_scraped.isoformat() if self.date_scraped else None
         }
 
-class ScrapingLog(https://www.globes.co.il/GlobesBoard/			):
+class ScrapingLog(db.Model):
+    __tablename__ = 'scraping_logs'
+
     id = db.Column(db.Integer, primary_key=True)
     start_time = db.Column(db.DateTime, default=datetime.utcnow)
     end_time = db.Column(db.DateTime)
@@ -51,3 +55,17 @@ class ScrapingLog(https://www.globes.co.il/GlobesBoard/			):
     items_scraped = db.Column(db.Integer, default=0)
     items_new = db.Column(db.Integer, default=0)
     error_message = db.Column(db.Text)
+
+    def __repr__(self):
+        return f'<ScrapingLog {self.status}>'
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'start_time': self.start_time.isoformat(),
+            'end_time': self.end_time.isoformat() if self.end_time else None,
+            'status': self.status,
+            'items_scraped': self.items_scraped,
+            'items_new': self.items_new,
+            'error_message': self.error_message
+        }
